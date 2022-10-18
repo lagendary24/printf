8708d4e2506fb0e457bdae615b10b9fec20aca4c
Endy4elect <endy4elect@yahoo.com>
2022-10-18 06:08:12 -0700

    main.h

--- main.h
+++ main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
 #include <stdarg.h>
 
@@ -30,4 +30,5 @@ typedef struct code_format
 	int (*f)(va_list);
 } code_f;
 
-#endif /* MAIN_H */
+#endif /* _MAIN_H_ */
+
